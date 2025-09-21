e40a141f5a1cc0ab431dd189de933fbd5385326d
Jan Ivica <wesleymustdie@gmail.com>
2025-09-21 14:10:47 +0200

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: "./",
   plugins: [
     vue()
   ],
