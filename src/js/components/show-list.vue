fb95f556f91f99d50314b62d087118e7006275e9
Jan Ivica <wesleymustdie@gmail.com>
2025-09-23 16:28:54 +0200

    fix v-for key in search list

--- src/js/components/show-list.vue
+++ src/js/components/show-list.vue
@@ -1,6 +1,6 @@
 <template>
    <section class="show-list">
-      <show v-for="show in shows" :show="show" :key="show.id"></show>
+      <show v-for="show in shows" :show="show" :key="show.data.id"></show>
    </section>
 </template>
 
